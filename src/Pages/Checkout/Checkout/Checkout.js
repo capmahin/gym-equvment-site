03758debad4a85e1e46ba6f8122f6816bc0ed21f
capmahin <mahinulhuque1996@gmail.com>
2022-04-18 03:27:35 +0600

    solve Reload Problem and add signOut

---
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Checkout = () => {
+  return (
+    <div>
+      <h2>Please CheckOut yourBooking</h2>
+    </div>
+  );
+};
+
+export default Checkout;
--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,11 +1,11 @@
 import React from "react";
-import down from "../../../images/404.png";
+import sleep from "../../../images/404.png";
 
 const NotFound = () => {
   return (
     <div>
       <h2 className="text-secondary text-center">Rest the Muscle</h2>
-      <img className="w-100 " src={down} alt="" />
+      <img className="w-100 " src={sleep} alt="" />
     </div>
   );
 };
