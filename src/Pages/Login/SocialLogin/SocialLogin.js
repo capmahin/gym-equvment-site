1f65874b7860b9e01d678ac3659b113285292f2d
capmahin <mahinulhuque1996@gmail.com>
2022-04-18 04:47:47 +0600

    fix error

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,8 +1,26 @@
 import React from "react";
 import google from "../../../images/social/google.png";
 import github from "../../../images/social/github.png";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  let errorElement;
+
+  if (error) {
+    errorElement = (
+      <div>
+        <p className="text-danger">Error: {error.message}</p>
+      </div>
+    );
+  }
+
+  if (user) {
+    navigate("/home");
+  }
   return (
     <div>
       <div className="d-flex align-items-center">
@@ -10,8 +28,12 @@ const SocialLogin = () => {
         <p className="mt-2 px-2">or</p>
         <div style={{ height: "1px" }} className="bg-secondary w-50"></div>
       </div>
+      {errorElement}
       <div>
-        <button className="btn btn-info w-50 d-block mx-auto my-2">
+        <button
+          onClick={() => signInWithGoogle()}
+          className="btn btn-info w-50 d-block mx-auto my-2"
+        >
           <img src={google} alt="" />
           <span className="px-2">Google Sign in</span>
         </button>
