38ad59c258abc5dfbb106413c25c7ce9e1633a21
capmahin <mahinulhuque1996@gmail.com>
2022-04-18 15:23:41 +0600

    add bio in about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,20 @@
 import React from "react";
+import me from "../../images/me.jpg";
 
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <h1>Name:Mahin-Ul Huque Mridha</h1>
+      <img className="w-50" src={me} alt="" />
+      <p>
+        I want to be a Full stack Developer. Much Concentrate in Front End
+        Development. Focus on up coming Modules , my next Target is Find a job
+        as a Web Developer . Practice the problems Regular and concentrated in
+        web Development.. LAlways focus on 1.Be comfortable with browser
+        programming using HTML, CSS, and JavaScript. 2. Dive deeper into Deep
+        Learning. 3. Practice with examples. 4.Make something new. and Goal
+        Achieve
+      </p>
     </div>
   );
 };
